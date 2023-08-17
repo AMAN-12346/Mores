38649d78b864d706dca704ff47246a6a044e7259
Manish Tomar <manish.tomar@moondive.co>
2023-08-17 15:46:41 +0530

    otp verify

--- pages/otpVerify/index.js
+++ pages/otpVerify/index.js
@@ -20,14 +20,49 @@ const LoginUser = () => {
           />
         </div>
         <h1 className="text-2xl font-bold">Verify OTP</h1>
-        <p className="mt-2">
-          Enter the 5 digit code received on your email address.
-        </p>
-        <p className="mt-6">____ ____ ____ ____</p>
-        <p className="mt-6">Not get code yet? Resend</p>
-        <button className="w-9/12 bg-button text-white py-2 rounded-lg mt-6">
-            Verify
-          </button>
+<p className="mt-2">Enter the 5 digit code received on your email address.</p>
+<div className="flex mt-6 w-9/12">
+  <input
+    type="text"
+    maxLength="1"
+    inputmode="numeric"
+    pattern="[0-9]"
+    placeholder="0"
+    className="w-1/5 px-2 py-1 text-center border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+  />
+  <input
+    type="text"
+    maxLength="1"
+    inputmode="numeric"
+    pattern="[0-9]"
+    placeholder="0"
+    className="w-1/5 ml-2 px-2 py-1 text-center border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+  />
+  <input
+    type="text"
+    maxLength="1"
+    inputmode="numeric"
+    pattern="[0-9]"
+    placeholder="0"
+    className="w-1/5 ml-2 px-2 py-1 text-center border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+  />
+  <input
+    type="text"
+    maxLength="1"
+    inputmode="numeric"
+    pattern="[0-9]"
+    placeholder="0"
+    className="w-1/5 ml-2 px-2 py-1 text-center border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+  />
+
+</div>
+<p className="mt-6">
+  <span className="text-gray-500">Not get code yet?</span>
+  <a href="#" className="text-blue-500 ml-1 hover:underline">Resend</a>
+</p>
+<button className="w-9/12 bg-button text-white py-2 rounded-lg mt-6">Verify</button>
+
+
 
         <div className="mt-0">
           <Image
