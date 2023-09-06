609f7c7bcced833ebb0effe61aa4ab1c993c2866
Priyanshu Vishwakarma <priyanshu.vishwakarma@moondive.co>
2023-09-06 11:05:05 +0530

    saved new changes

--- components/HomePage/SearchSection/FilterButtons.jsx
+++ components/HomePage/SearchSection/FilterButtons.jsx
@@ -4,7 +4,7 @@ const FilterButtons=({name, icon})=>{
     const[active,toggleactive]=useState(true)
     return(
         <div className={`flex`}>
-        <div className={`flex gap-2 px-4 py-2 border border-solid border-white rounded-full text-white ${active ?'':'bg-primary border-transparent'}`} >
+        <div className={`flex gap-2 px-4 py-2 border border-solid rounded-full text-white ${active ?'':'bg-primary border border-transparent'}`} >
         <div className='bg-white rounded-full p-1'>
             <Image
             src={icon}
