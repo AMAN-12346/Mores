4f73252e092fe61e2fb6c7707dc405d2c1597118
Priyanshu Vishwakarma <priyanshu.vishwakarma@moondive.co>
2023-09-07 15:17:29 +0530

    padding for button fixed in the search filters

--- components/HomePage/SearchSection/FilterButtons.jsx
+++ components/HomePage/SearchSection/FilterButtons.jsx
@@ -18,7 +18,7 @@ const FilterButtons=({name, icon,isMobileView})=>{
       </div>
       ) :
       (<div className='flex mx-4'>
-      <div className={`flex gap-2 px-1 py-2 border border-solid rounded-full text-white text-xs ${active ?'':'bg-primary border border-transparent'} `} >
+      <div className={`flex gap-2 px-1 py-1 border border-solid rounded-full text-white text-xs ${active ?'':'bg-primary border border-transparent'} `} >
       <div className='bg-white rounded-full p-1'>
           <Image
           src={icon}
