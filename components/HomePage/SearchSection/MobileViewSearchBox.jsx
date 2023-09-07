78fec37ed0da3cfa215fe855757d4b81176dca57
Priyanshu Vishwakarma <priyanshu.vishwakarma@moondive.co>
2023-09-07 17:31:17 +0530

    rounded search box

--- components/HomePage/SearchSection/MobileViewSearchBox.jsx
+++ components/HomePage/SearchSection/MobileViewSearchBox.jsx
@@ -3,12 +3,12 @@ import magnify from '../../../assets/SearchBoxIcon/magnifyglass.svg';
 
 const MobileViewSearchBox = () => {
   return (
-    <div className="flex text-xs items-center p-1 bg-white rounded-md">
+    <div className="flex text-xs gap-10 items-center p-1 bg-white rounded-full">
       <input
         className="w-[240px] rounded-l-md outline-none placeholder-gray-500"
         placeholder="Search by location, Project Name"
       />
-      <div className="bg-primary rounded-md px-2 py-1 items-center">
+      <div className="bg-primary px-2 py-1 items-center rounded-full ">
         <button className="rounded-md text-white focus:outline-none items-center">
           <Image src={magnify} width={9} height={9} alt='icon' />
         </button>
