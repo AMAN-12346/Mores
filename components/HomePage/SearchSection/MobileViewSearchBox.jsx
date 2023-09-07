3432db1f87a2a172dc8cbd948006e16533515d9e
Priyanshu Vishwakarma <priyanshu.vishwakarma@moondive.co>
2023-09-07 16:55:11 +0530

    minor fix in searchbox (padding)

--- components/HomePage/SearchSection/MobileViewSearchBox.jsx
+++ components/HomePage/SearchSection/MobileViewSearchBox.jsx
@@ -3,14 +3,14 @@ import magnify from '../../../assets/SearchBoxIcon/magnifyglass.svg';
 
 const MobileViewSearchBox = () => {
   return (
-    <div className="flex text-xs items-center p-3 bg-white rounded-md">
+    <div className="flex text-xs items-center p-1 bg-white rounded-md">
       <input
         className="w-[240px] rounded-l-md outline-none placeholder-gray-500"
         placeholder="Search by location, Project Name"
       />
-      <div className="bg-primary rounded-md p-2">
-        <button className="px-2  rounded-md text-white focus:outline-none items-center">
-          <Image src={magnify} alt='icon' />
+      <div className="bg-primary rounded-md px-2 py-1 items-center">
+        <button className="rounded-md text-white focus:outline-none items-center">
+          <Image src={magnify} width={9} height={9} alt='icon' />
         </button>
       </div>
     </div>
