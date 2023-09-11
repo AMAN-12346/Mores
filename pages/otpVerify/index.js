5bd6194be90231d989806ccaa1b3a05e9dcd9362
Manish Tomar <manish.tomar@moondive.co>
2023-09-11 18:04:10 +0530

    chnaged

--- pages/otpVerify/index.js
+++ pages/otpVerify/index.js
@@ -123,6 +123,9 @@ const VerifyOTP = () => {
   };
   const handleResendOTP = async () => {
     const userID = localStorage.getItem("userID");
+    if (!userID){
+      return
+    }
 
     // If the userID starts with "+91", it's a mobile number
     const isMobileNumber = userID.startsWith("+91");
@@ -187,7 +190,7 @@ const VerifyOTP = () => {
         <div className={styles.formContainer}>
           <h1 className={styles.heading}>Verify OTP</h1>
           <p className={styles.slogan}>Enter the 6 digit code you received.</p>
-          <div className=" flex mt-3 w-72 -ml-3">
+          <div className="flex mt-3 w-72 -ml-3">
             {otp.map((value, index) => (
               <input
                 key={index}
@@ -196,7 +199,13 @@ const VerifyOTP = () => {
                 inputMode="numeric"
                 pattern="[0-9]"
                 placeholder="0"
-                className="w-1/5 ml-2 px-2 py-1 text-center border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+                className="w-1/5 ml-2 px-2 py-1 text-center"
+                style={{
+                  background: "none", // Remove the background
+                  border: "none", // Remove the default border
+                  borderBottom: "2px solid #333", // Add your desired dark bottom border style here
+                  outline: "none", // Remove the focus outline
+                }}
                 value={value}
                 onChange={(e) => handleOTPChange(index, e.target.value)}
                 onKeyDown={(e) => handleKeyDown(e, index)}
--- pages/user/addProperty/component/AdditionalDetailsForm.js
+++ pages/user/addProperty/component/AdditionalDetailsForm.js
@@ -22,23 +22,23 @@ const AdditionalDetailsForm = ({
 
   const [uploadMessage, setUploadMessage] = useState(""); // State for upload message
 
-  const [additionalRooms, setAdditionalRooms] = useState([]);
+  // const [additionalRooms, setAdditionalRooms] = useState([]);
 
-  const [possessionStatus, setPossessionStatus] = useState("");
-  const [furnishStatus, setFurnishStatus] = useState("");
-  const [propertyAge, setPropertyAge] = useState("");
-  const [additionalBalconies, setAdditionalBalconies] = useState([]);
-  const [balconyView, setBalconyView] = useState(""); // State for Balcony View
-  const [viewOption, setViewOption] = useState("");
-  const [flooringOption, setFlooringOption] = useState("");
-  const [floorNumber, setFloorNumber] = useState("");
-  const [towerBlock, setTowerBlock] = useState("");
-  const [totalFloors, setTotalFloors] = useState("");
-  const [unitNumber, setUnitNumber] = useState("");
-  const [privateBalcony, setPrivateBalcony] = useState(false);
-  const [numBedrooms, setNumBedrooms] = useState("");
-  const [numBathrooms, setNumBathrooms] = useState("");
-  const [powerBackupOption, setPowerBackupOption] = useState("");
+  // const [possessionStatus, setPossessionStatus] = useState("");
+  // const [furnishStatus, setFurnishStatus] = useState("");
+  // const [propertyAge, setPropertyAge] = useState("");
+  // const [additionalBalconies, setAdditionalBalconies] = useState([]);
+  // const [balconyView, setBalconyView] = useState(""); // State for Balcony View
+  // const [viewOption, setViewOption] = useState("");
+  // const [flooringOption, setFlooringOption] = useState("");
+  // const [floorNumber, setFloorNumber] = useState("");
+  // const [towerBlock, setTowerBlock] = useState("");
+  // const [totalFloors, setTotalFloors] = useState("");
+  // const [unitNumber, setUnitNumber] = useState("");
+  // const [privateBalcony, setPrivateBalcony] = useState(false);
+  // const [numBedrooms, setNumBedrooms] = useState("");
+  // const [numBathrooms, setNumBathrooms] = useState("");
+  // const [powerBackupOption, setPowerBackupOption] = useState("");
 
   const handlePhotoSelect = () => {
     const input = document.createElement("input");
@@ -371,7 +371,7 @@ const AdditionalDetailsForm = ({
               onPhotoUpload={photoUploadLoading}
             />
           </div>
-          <div className="flex justify-end ">
+          <div className="flex justify-end">
             <div className="grid grid-cols-3 gap-2">
               {photoURLs.slice(0, 5).map((photoURL, index) => (
                 <div key={`photo_${index}`} className="mb-2">
@@ -385,9 +385,9 @@ const AdditionalDetailsForm = ({
           </div>
           {photoURLs.length > 5 && (
             <div className="flex justify-end  mt-2">
-              <div className="grid grid-cols-3 gap-2">
+              <div className="grid grid-cols-3 gap-2 h-2">
                 {photoURLs.slice(5).map((photoURL, index) => (
-                  <div key={`photo_${index}`} className="mb-2">
+                  <div key={`photo_${index}`} className="mb-2 h-2">
                     <img src={photoURL.url} alt={`Uploaded Photo ${index}`} />
                     <button onClick={() => deletePhoto(index,photoURL.url)}>
                       <FontAwesomeIcon icon={faTrash} /> {/* Trash icon */}
