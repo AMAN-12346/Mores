9c7efd418aa1b2d7861b2c8de343abf39f39a017
Prateek Rai <prateek.rai@moondive.co>
2023-09-12 17:48:46 +0530

    saved

--- pages/career/[name]/index.js
+++ pages/career/[name]/index.js
@@ -8,9 +8,27 @@ const index = () => {
     const [data, setData] = useData();
    console.log("====", data)
     return (
-        <div className='bg-[#fdf8f1] animate__animated animate__fadeInRight'>
+        <div className='bg-white animate__animated animate__fadeInRight'>
             <div className={Styles.mainDiv}>
-                <h1>Apply</h1>
+                <div className='m-auto w-[50%]'>
+                    <h1 className={Styles.heading}>Apply Here</h1>
+                    <p className={Styles.para}>excited to explore opportunities at Mores and be a part of our dynamic team</p>
+                    <hr className={Styles.underline}/>
+                </div>                     
+            </div>
+            <div className='w-[50%] m-auto'>
+               <h1 className={Styles.jobheading}>{data.title}</h1>
+                <p className={Styles.jobpara}>Salary : Rs. ----/year</p>
+                <p className={Styles.jobpara}>Gender : {data.gender}</p>
+                <p className={Styles.jobpara}>Expertise : {data.experience}</p>
+                <div className={Styles.description}>Description</div>
+                <p className={Styles.jobpara}>{data.description}</p>
+                <div className={Styles.description}>Qualification</div>
+                <ul>
+                    {data?.qualification?.map((line)=>
+                    <li className={Styles.jobpara}>⦿ {line}</li>
+                    )}
+                </ul>
             </div>
         </div> 
     );
