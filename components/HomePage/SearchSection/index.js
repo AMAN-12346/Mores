f1903a87bcce1aa77b12aa256a4d092a48183155
Priyanshu Vishwakarma <priyanshu.vishwakarma@moondive.co>
2023-09-13 15:55:48 +0530

    nearby search working

--- components/HomePage/SearchSection/index.js
+++ components/HomePage/SearchSection/index.js
@@ -8,16 +8,18 @@ import findagent from '../../../assets/SearchFilterIcons/findagent.svg'
 import moreoptions from '../../../assets/SearchFilterIcons/moreoptions.svg'
 import SearchBox from "./SearchBox";
 import useWindowWidth from "@/context/useWindowWidth";
+import Image from "next/image";
 const SearchSection = () => {
   const windowWidth = useWindowWidth();
   const isMobileView = windowWidth < 768; // Adjust breakpoint as needed
 
   return (
+    <>
     <div className={`h-screen flex flex-col md:p-10 justify-center max-lg:h-[500px] ${isMobileView ? "h-full" : ""}`} style={{ background: 'url(https://s3-alpha-sig.figma.com/img/3754/edfd/8536a63136da16ed89a1c3e0133959ae?Expires=1695600000&Signature=DHPhrzOnRbdMWQavcj0LYHjBjvfLFgjL0XRnmpcP2NzWCl3p3HbeeehfcHooDvP9hhgUlhYYawNhwf~qANga2lN71IDq1qyqJmJhkUXjGAerQmLPUfqydL50~CGZ60iJ92TkJpinIIEx-4ek4hfG36KSZAzi3Ugmqr~oOZI0bskadNYLqY-4AQ7LSEQT~6yshADjKnc4hzX46iE9-HbvZKDclIIJlqDmgMGNGHc4jTjqcvg1X36axmob-g-RDfbNW3HRqgA5r5IeeiTag-mgnoOB0m1qSdolaVZ86Tr4Zi9tZe-KaXZnqBUGy9fvpKeiKtotrTyFdp549Yam9rEgAg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4) no-repeat', backgroundSize: 'cover' }}>
   <div className="items-center 2xl:ml-[400px]">
   <h1 className={`lg:text-5xl lg:text-white lg:font-custom md:text-custom md:text-white md:font-custom lg:pl-24 ${isMobileView ? 'text-4xl px-16 font-semibold text-center text-white leading-tight' : 'md:text-custom2 ml-6'}`}>Discover Your New Home</h1>
   <div className="lg:w-2/3 lg:pl-24 lg:mt-4 md:ml-5">
-    <p className={`lg:text-xl lg:text-white lg:font-custom1 md:text-custom1 md:text-white md:font-custom1 ${isMobileView ? 'text-base px-7 leading-relaxed text-white font-light text-center mt-4' : 'md:text-custom3 ml-2 md:w-[600px]'}`}>You can get your desired awesome properties, homes, condos, etc., here by name, category, or location.</p>
+    <p className={`lg:text-sm lg:text-white lg:font-custom1 md:text-custom1 md:text-white md:font-custom1 ${isMobileView ? 'text-base px-7 leading-relaxed text-white font-light text-center mt-4' : 'md:text-custom3 ml-2 md:w-[600px]'}`}>You can get your desired awesome properties, homes, condos, etc., here by name, category, or location.</p>
   </div>
 </div>
 
@@ -57,10 +59,12 @@ const SearchSection = () => {
       <div className={`flex lg:w-full lg:justify-center lg:mb-20 ${isMobileView ? 'justify-center mt-4' : ''}`}>
         <SearchBox isMobileView={isMobileView} />
       </div>
-      <div className="w-full h-[100px] " style={{ background: 'https://s3-alpha-sig.figma.com/img/c17e/3503/c6f0c48549da8bd6d54100077e8244bf?Expires=1695600000&Signature=gIswu-bEWX95uekdVxxFNITvxTTuWZEJXiErW2k9ZL~~jbnmS0FtM0424nP~ipLN5OxI2vlrLlItO9Nfa5kkKnJzSEONxVfv0~vBpsqagScDH8jeu6COj7UGxneUF-cKDppLKapLi3DnpAe1Fegt2WCRhnNBMvc4i6LeYovFCEDew~~kgI0BR0Lnj56cAS~xswZAnztMeGER-ObV310QOo7mgPE3zr-NZMKvco-7f8Hs9pd8K51pGWkDwyXvfvJr7hmjhUf26NgmHvIbLJdqOvJ1KSI9CeTsQswhikRE3heNTynBQxDAim82Gu6vEo5K1IiQ87zrj5rPNmNCjgBhCA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4) ', backgroundSize: 'cover'}}>
-
-      </div>
+      {/* <div className="w-full h-[100px]" style={{ background:'url(https://s3-alpha-sig.figma.com/img/c17e/3503/c6f0c48549da8bd6d54100077e8244bf?Expires=1695600000&Signature=gIswu-bEWX95uekdVxxFNITvxTTuWZEJXiErW2k9ZL~~jbnmS0FtM0424nP~ipLN5OxI2vlrLlItO9Nfa5kkKnJzSEONxVfv0~vBpsqagScDH8jeu6COj7UGxneUF-cKDppLKapLi3DnpAe1Fegt2WCRhnNBMvc4i6LeYovFCEDew~~kgI0BR0Lnj56cAS~xswZAnztMeGER-ObV310QOo7mgPE3zr-NZMKvco-7f8Hs9pd8K51pGWkDwyXvfvJr7hmjhUf26NgmHvIbLJdqOvJ1KSI9CeTsQswhikRE3heNTynBQxDAim82Gu6vEo5K1IiQ87zrj5rPNmNCjgBhCA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)', backgroundSize: 'cover'}}>
+        
+      </div> */}
     </div>
+     <Image src='https://s3-alpha-sig.figma.com/img/c17e/3503/c6f0c48549da8bd6d54100077e8244bf?Expires=1695600000&Signature=gIswu-bEWX95uekdVxxFNITvxTTuWZEJXiErW2k9ZL~~jbnmS0FtM0424nP~ipLN5OxI2vlrLlItO9Nfa5kkKnJzSEONxVfv0~vBpsqagScDH8jeu6COj7UGxneUF-cKDppLKapLi3DnpAe1Fegt2WCRhnNBMvc4i6LeYovFCEDew~~kgI0BR0Lnj56cAS~xswZAnztMeGER-ObV310QOo7mgPE3zr-NZMKvco-7f8Hs9pd8K51pGWkDwyXvfvJr7hmjhUf26NgmHvIbLJdqOvJ1KSI9CeTsQswhikRE3heNTynBQxDAim82Gu6vEo5K1IiQ87zrj5rPNmNCjgBhCA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4' width={100} height={100} alt="bottom-image" />
+    </>
   );
 };
 
