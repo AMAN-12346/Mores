5a2dcc38d8b5c480270a2d85084c00cb724bf218
Prateek Rai <prateek.rai@moondive.co>
2023-09-13 10:59:28 +0530

    saved

--- pages/career/[name]/index.js
+++ pages/career/[name]/index.js
@@ -13,20 +13,30 @@ const index = () => {
                 <div className='m-auto w-[50%]'>
                     <h1 className={Styles.heading}>Apply Here</h1>
                     <p className={Styles.para}>excited to explore opportunities at Mores and be a part of our dynamic team</p>
-                    <hr className={Styles.underline}/>
                 </div>                     
             </div>
             <div className='w-[50%] m-auto'>
-               <h1 className={Styles.jobheading}>{data.title}</h1>
-                <p className={Styles.jobpara}>Salary : Rs. ----/year</p>
-                <p className={Styles.jobpara}>Gender : {data.gender}</p>
-                <p className={Styles.jobpara}>Expertise : {data.experience}</p>
+                
+                <div className='flex justify-between'>
+                    <h1 className={Styles.jobheading}>{data.title}</h1>
+                    <button className={Styles.jobButton}>Apply</button>
+                </div>
+                <p className={Styles.jobpara}><span style={{color:"#018191"}}>Salary :</span> Rs. ----/year</p>
+                <p className={Styles.jobpara}><span style={{color:"#018191"}}>Gender :</span> {data.gender}</p>
+                <p className={Styles.jobpara}><span style={{color:"#018191"}}>Job Type :</span> {data.type}</p>
+                <p className={Styles.jobpara}><span style={{color:"#018191"}}>Expertise :</span> {data.experience}</p>
+                <div className={Styles.description}>Requirements</div>
+                <ul className=''>
+                    {data?.requirements?.map((points)=>
+                    <li className={Styles.requirementPoint}>{points}</li>
+                    )}
+                </ul>
                 <div className={Styles.description}>Description</div>
                 <p className={Styles.jobpara}>{data.description}</p>
                 <div className={Styles.description}>Qualification</div>
                 <ul>
                     {data?.qualification?.map((line)=>
-                    <li className={Styles.jobpara}>⦿ {line}</li>
+                    <li className={Styles.jobpara}>● {line}</li>
                     )}
                 </ul>
             </div>
